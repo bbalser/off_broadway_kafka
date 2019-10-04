2f1af592d9142e7d590898f0e33ca7680529a73f
Brian Balser <bbalser@pillartechnology.com>
2019-10-04 10:17:16 -0400

    Bump Version to 0.4.0

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule OffBroadwayKafka.MixProject do
   def project do
     [
       app: :off_broadway_kafka,
-      version: "0.3.1",
+      version: "0.4.0",
       elixir: "~> 1.8",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
